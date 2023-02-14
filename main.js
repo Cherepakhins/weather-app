da50bf8f4db4af63457dc8357430d229450c57e6
Anton Cherepakhins <114250752+Cherepakhins@users.noreply.github.com>
2023-02-14 16:35:49 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const now = day + " " + time;
 const hours = today.getHours();
 
 function removeCard () {
-    const prevCard = document.querySelector('.card');
+    const prevCard = document.querySelector('.card-weather');
     if(prevCard) prevCard.remove();
 }
 
@@ -26,15 +26,11 @@ function removeCard () {
 form.onsubmit = function (e) {
     // Отменяем отправку формы
     e.preventDefault();
-
     // Берем значения из инпута и обрезаем пробелы 
     let city = input.value.trim();
-
     // Делаем запрос на сервер
     // Адрес запроса
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=ru`;
-
-    
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=ru`;  
     //Выполняем запрос
     fetch(url).then((response) => {
         return response.json()
@@ -53,21 +49,23 @@ form.onsubmit = function (e) {
         const wDescription = weatherDescription.replace(weatherDescription[0], weatherDescription[0].toUpperCase());
          // Разметка для карточки
          const html = `
-            <div class="card">
-                <h2 class="card__title-city">${data.name}<span class="card__GB">${data.sys.country}</span></h2>
+            <div class="card card-weather">
                 <div class="today">
-                    <span class='clock'>${day}</span>
+                    <span class='day'>${day}</span>
                     <span class='clock'>${time}</span>
                 </div>
+                <h2 class="card__title">${data.name}<span class="card__GB">${data.sys.country}</span></h2>
+                
                 <div class="card__weather">
                     <p class="card__value">${Math.round(data.main.temp)}<sup>°c</sup> </p>
                     <div class="box">
                         <img src="https://openweathermap.org/img/wn/${data.weather['0'].icon}@4x.png" alt="weather" class="card__img">
                     </div>
                 </div>
-                <div class="card__description-cloud">${wDescription}</div>
+                <div class="card__description">${wDescription}</div>
             </div>`;
         main.insertAdjacentHTML('afterbegin', html);
+        console.log(data)
         //Очищаем поле ввода и возвращаем на него фокус
         input.value = '';
         input.focus();
@@ -86,3 +84,25 @@ if(19 < hours && hours < 24 || hours < 7){
         localStorage.setItem('darkMode', 'light'); 
     }
 }
+
+const apiKeyNasa = '9S5gSnfoynIPdjs82VwzDL7G5KlejUhrnjRDKBzq';
+const urlNasa = `https://api.nasa.gov/planetary/apod?api_key=${apiKeyNasa}`;  
+    //Выполняем запрос
+    fetch(urlNasa).then((response) => {
+        return response.json()
+     }).then((data) => {
+        {
+            console.log(data);
+        // Разметка для карточки
+        const html = `
+            <div class="card-nasa">
+                <div class="card-nasa-wrapper">
+                    <h2 class="card__title card__title-nasa">${data.title}</h2>
+                    <span class='card__description card__description-nasa'>${data.explanation}</span>
+                    <img src="${data.url}" alt="space" class="img-space">
+                </div>
+                
+            </div>`;
+        main.insertAdjacentHTML('beforeend', html);
+    }});
+
